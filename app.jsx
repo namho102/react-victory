f90c5dcdfc98b596122a7a131a999d1c2ab13dfc
Hoang Nam <namhoag.td09@gmail.com>
2016-01-31 14:47:55 +0700

    add changes

--- app.jsx
+++ app.jsx
@@ -5,7 +5,6 @@ import { VictoryBar } from 'victory';
 import * as lib from './lib';
 
 var arr = lib.shuffle(lib.generateArray(50));
-console.log(arr);
 
 class BarChart extends Component {
   constructor(props) {
@@ -16,12 +15,10 @@ class BarChart extends Component {
   }
 
   getData() {
-    // return lib.generateData(100);
     return lib.arrayToData(arr.bubblesort());
   }
     
   componentDidMount() {
-    // arr.bubblesort()
     setInterval(() => {
       this.setState({
         data: this.getData(),
@@ -36,7 +33,6 @@ class BarChart extends Component {
             domain={{y: [0, 50]}}
             width={600}
             height={600}
-            // data={arr}
             data={this.state.data}
             dataAttributes={[
                 {fill: "cornflowerblue"}
@@ -47,14 +43,3 @@ class BarChart extends Component {
 }
 
 React.render(<BarChart />, document.getElementById('app'));
-
-// arr.bubblesort();
-
-setTimeout(() => {
-    React.render(<BarChart />, document.getElementById('app'));
-}, 0);
-
-// setTimeout(() => {
-//     console.log('begin sorting');
-//     arr.bubblesort();
-// }, 3000);
